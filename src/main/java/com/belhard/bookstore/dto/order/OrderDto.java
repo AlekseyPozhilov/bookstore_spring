6e5d21ea92bc4b549e7cec6d7d954300d754c33d
Aleksey Pozhilov <lpozhilov17@mail.ru>
2024-01-25 23:27:52 +0300

    Corrected OrderDto

--- src/main/java/com/belhard/bookstore/dto/order/OrderDto.java
+++ src/main/java/com/belhard/bookstore/dto/order/OrderDto.java
@@ -1,8 +1,10 @@
 package com.belhard.bookstore.dto.order;
 
+import com.belhard.bookstore.entity.OrderInfo;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Data
 public class OrderDto {
@@ -10,6 +12,7 @@ public class OrderDto {
     private Long userId;
     private BigDecimal totalCost;
     private Status status;
+    private List<OrderInfo> items;
 
     public enum Status {
         PENDING, PAID, DELIVERED, CANCELED;
