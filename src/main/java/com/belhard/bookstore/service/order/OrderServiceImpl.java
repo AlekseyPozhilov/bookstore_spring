f025db22b45fb2b024c60d7279f9fe2b2fa65a1f
Aleksey Pozhilov <lpozhilov17@mail.ru>
2024-01-29 15:42:49 +0300

    Corrected OrderServiceImpl

--- src/main/java/com/belhard/bookstore/service/order/OrderServiceImpl.java
+++ src/main/java/com/belhard/bookstore/service/order/OrderServiceImpl.java
@@ -1,16 +1,18 @@
 package com.belhard.bookstore.service.order;
 
-import com.belhard.bookstore.Repository.order.OrderRepository;
 import com.belhard.bookstore.dto.book.BookDto;
-import com.belhard.bookstore.dto.order.OrderDto;
 import com.belhard.bookstore.entity.Book;
+import com.belhard.bookstore.repository.order.OrderRepository;
+import com.belhard.bookstore.dto.order.OrderDto;
 import com.belhard.bookstore.entity.Order;
-import com.belhard.bookstore.service.book.BookServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Log4j2
 @RequiredArgsConstructor
@@ -20,64 +22,70 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public OrderDto findById(Long id) {
-        Order order = orderRepository.findByID(id);
+        Order order = orderRepository.findById(id);
         if (order == null) {
             throw new RuntimeException("No book with id = " + id);
         }
-        return orderReadDto(order);
+        return mapToDto(order);
     }
 
     @Override
     public List<OrderDto> findAll() {
-        return orderRepository.findAll().stream().map(OrderServiceImpl::orderReadDto).toList();
+        return orderRepository.findAll()
+                .stream()
+                .map(this::mapToDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public void delete(Long id) {
         log.debug("Deleting order: {}", id);
+
         orderRepository.delete(id);
+
+        log.debug("Order deleted: {}", id);
     }
 
     @Override
     public OrderDto create(OrderDto dto) {
         log.debug("Creating order: {}", dto);
-
-        Order order = toEntity(dto);
+        Order order = mapToEntity(dto);
         Order created = orderRepository.create(order);
-
-        log.debug("Order created: {}", order);
-
-        return orderReadDto(created);
+        log.debug("Order created: {}", created);
+        return mapToDto(created);
     }
 
-    @Override
-    public OrderDto update(OrderDto dto) {
-        log.debug("Updating order: {}", dto);
+@Override
+public OrderDto update(OrderDto dto) {
+    log.debug("Updating order: {}", dto);
 
-        OrderDto orderDto = findById(dto.getId());
-        orderDto.setTotalCost(dto.getTotalCost());
-        orderDto.setStatus(dto.getStatus());;
+    Order order = orderRepository.findById(dto.getId());
 
-        Order order = toEntity(orderDto);
-        Order edited = orderRepository.update(order);
-
-        log.debug("Order updated: {}", orderDto);
-
-        return orderReadDto(edited);
+    order.setTotalCost(dto.getTotalCost());
+    OrderDto.Status status = dto.getStatus();
+    if (status != null) {
+        order.setStatus(Order.Status.valueOf(status.toString()));
     }
 
-    public static OrderDto orderReadDto(Order orderEntity) {
-        OrderDto dto = new OrderDto();
-        dto.setId(orderEntity.getId());
-        dto.setTotalCost(orderEntity.getTotalCost());
-        Order.Status status = orderEntity.getStatus();
-        if (status != null) {
-            dto.setStatus(OrderDto.Status.valueOf(status.toString()));
-        }
-        return dto;
-    }
+    Order edited = orderRepository.update(order);
 
-    public static Order toEntity(OrderDto dto) {
+    log.debug("Order updated: {}", edited);
+
+    return mapToDto(edited);
+}
+
+private OrderDto mapToDto(Order order) {
+    OrderDto dto = new OrderDto();
+    dto.setId(order.getId());
+    dto.setTotalCost(order.getTotalCost());
+    Order.Status status = order.getStatus();
+    if (status != null) {
+        dto.setStatus(OrderDto.Status.valueOf(status.toString()));
+    }
+    return dto;
+}
+
+    private Order mapToEntity(OrderDto dto) {
         Order orderEntity = new Order();
         orderEntity.setId(dto.getId());
         orderEntity.setTotalCost(dto.getTotalCost());
