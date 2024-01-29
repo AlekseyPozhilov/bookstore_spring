08df9b8b2cff21488a09ba4e742eba1b90ff17d5
Aleksey Pozhilov <lpozhilov17@mail.ru>
2024-01-29 15:45:08 +0300

    Corrected FrontController

--- src/main/java/com/belhard/bookstore/controller/FrontController.java
+++ src/main/java/com/belhard/bookstore/controller/FrontController.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
 
 import java.io.IOException;
 
