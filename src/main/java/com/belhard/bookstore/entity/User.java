7bd9349b7da2ebf9988e0b28ceddd5a2d5780daf
Aleksey Pozhilov <lpozhilov17@mail.ru>
2024-01-29 15:43:30 +0300

    Corrected entity user

--- src/main/java/com/belhard/bookstore/entity/User.java
+++ src/main/java/com/belhard/bookstore/entity/User.java
@@ -1,15 +1,40 @@
 package com.belhard.bookstore.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "firstName")
     private String firstName;
+
+    @Column(name = "lastName")
     private String lastName;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "dateOfBirth")
     private String dateOfBirth;
+
+    @Column(name = "gender")
     private String gender;
+
+    @Column(name = "phoneNumber")
     private String phoneNumber;
+
+    @Column(name = "password")
     private String password;
 }
