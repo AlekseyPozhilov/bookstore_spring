4a0ab6a63d1052dcc5201eeb9672a9b643692e82
Aleksey Pozhilov <lpozhilov17@mail.ru>
2024-01-29 15:44:29 +0300

    Corrected DataSourceImpl

--- src/main/java/com/belhard/bookstore/connection/DataSourceImpl.java
+++ src/main/java/com/belhard/bookstore/connection/DataSourceImpl.java
@@ -2,15 +2,11 @@ package com.belhard.bookstore.connection;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
-
-import javax.annotation.PostConstruct;
 import java.io.Closeable;
 import java.sql.Connection;
 
 @Log4j2
-@Component
 public class DataSourceImpl implements DataSource, Closeable {
     @Value("${my.app.db.${my.app.profile}.url}")
     private String url;
@@ -25,13 +21,14 @@ public class DataSourceImpl implements DataSource, Closeable {
     private String drv;
 
     @Value("${my.app.db.${my.app.profile}.poolSize}")
-    private int pooolSize;
+    private String poolSizeString;
+    private int poolSize;
 
     private ConnectionPool connectionPool;
 
-    @PostConstruct
     public void init() {
-        connectionPool = new ConnectionPool(url, user, password, drv, pooolSize);
+        poolSize = Integer.parseInt(poolSizeString);
+        connectionPool = new ConnectionPool(url, user, password, drv, poolSize);
     }
 
     public Connection getConnection() {
@@ -39,7 +36,7 @@ public class DataSourceImpl implements DataSource, Closeable {
             log.info("Connection to the database is being established...");
             Class.forName("org.postgresql.Driver");
             if (connectionPool == null) {
-                connectionPool = new ConnectionPool(url, user, password, drv, pooolSize);
+                connectionPool = new ConnectionPool(url, user, password, drv, poolSize);
             }
             return connectionPool.getConnection();
         } catch (ClassNotFoundException e) {
