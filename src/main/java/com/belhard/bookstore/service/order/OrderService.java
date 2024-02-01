92f7a2c12435e09ea25663dd6a3ed26717273ffa
Aleksey Pozhilov <lpozhilov17@mail.ru>
2024-02-01 19:36:47 +0300

    Edited OrderService

--- src/main/java/com/belhard/bookstore/service/order/OrderService.java
+++ src/main/java/com/belhard/bookstore/service/order/OrderService.java
@@ -1,7 +1,6 @@
 package com.belhard.bookstore.service.order;
 
-import com.belhard.bookstore.dto.order.OrderDto;
-import com.belhard.bookstore.entity.Order;
+import com.belhard.bookstore.data.dto.order.OrderDto;
 
 import java.util.List;
 
