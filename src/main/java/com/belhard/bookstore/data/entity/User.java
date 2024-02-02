b7c5d75f5bfe588f6b71b208a2930745c870fc37
Aleksey Pozhilov <lpozhilov17@mail.ru>
2024-02-02 18:57:55 +0300

    Corrected entity User

--- src/main/java/com/belhard/bookstore/data/entity/User.java
+++ src/main/java/com/belhard/bookstore/data/entity/User.java
@@ -17,22 +17,22 @@ public class User {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "firstName")
+    @Column(name = "first_name")
     private String firstName;
 
-    @Column(name = "lastName")
+    @Column(name = "last_name")
     private String lastName;
 
     @Column(name = "email")
     private String email;
 
-    @Column(name = "dateOfBirth")
+    @Column(name = "date_of_birth")
     private String dateOfBirth;
 
     @Column(name = "gender")
     private String gender;
 
-    @Column(name = "phoneNumber")
+    @Column(name = "phone_number")
     private String phoneNumber;
 
     @Column(name = "password")
